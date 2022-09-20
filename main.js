311be14146fc17115b9cea4d3077fbce90b81fe1
sabrinaMac <sabrina.scherma@gmail.com>
2022-09-19 22:49:47 -0600

    set up file structure and built out html

---
+++ main.js
@@ -0,0 +1,19 @@
+let myLibrary = [];
+
+class Book {
+    constructor(
+        title = 'unknown',
+        author = 'unknown',
+        pages = '0',
+        isRead = false
+    ) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}}
+
+
+function addBookToLibrary() {
+
+}
